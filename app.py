ce1a7e805a2469ae87b0b5db770952ddd040c9f2
Aashutosh Barhate <aashutoshbarhate@gmail.com>
2025-10-18 13:10:03 +0530

    Done with html rendering

--- app.py
+++ app.py
@@ -4,18 +4,14 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'any_random_string'
+app.config['SECRET_KEY'] = 'any_random_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://aashutosh:your_password@localhost/leetcode_tracker'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'login'  
 
-@app.route('/')
-def home():
-    return "Flask + PostgreSQL connection working successfully 🚀"
-
-if __name__ == "__main__":
-    app.run(debug=True)
+from auth_routes import *
+from learning_routes import *
